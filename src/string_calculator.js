b2e479d4467958cff8d0b5bb1412fa0f504fd829
Jeanette <jbrasepae@gmail.com>
2020-05-20 16:44:20 +0200

    function added

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,4 +1,18 @@
-function add(){
+function add(str){
+    num = str
+    //str = num
+    if(str === ""){
+        return 0;
+    }
+    num = str.split(",")
+    var sum = 0;
+   // var arrayLength = num.length;
+    for(var i = 0; i < num.length; i++){
+        sum += parseInt(num[i])
+    }
+    return sum
 
+    
 }
+console.log(add("1,1"))
 module.exports = {add}
