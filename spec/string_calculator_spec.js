c5cae51e78ffe5d77b27f55238dba04d3a70a5a3
Jeanette <jbrasepae@gmail.com>
2020-05-26 00:44:21 +0200

    code fixed but not yet completed

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,52 +1,35 @@
-const {stringCalculator} = require("../src/string_calculator")
+const {add} = require("../src/string_calculator")
 
-describe('stringCalculator', function(){
+describe('add', function(){
     it('should return 0 for an empty string',function(){
-        expect(stringCalculator.add("")).toBe(0)
+        expect(add("")).toBe(0)
     })
     it('should input one number as a string',function(){
-        expect(stringCalculator.add("1")).toBe(1)
+        expect(add("1")).toBe(1)
     })
     it('should input two numbers as a string',function(){
-        expect(stringCalculator.add("1,1")).toBe(2)
+        expect(add("1,1")).toBe(2)
     })
     it('should input multiple numbers as a string',function(){
-        expect(stringCalculator.add("1,2,3,4")).toBe(10)
+        expect(add("1,2,3,4")).toBe(10)
     })
     it('should allow newlines',function(){
-        expect(stringCalculator.add("1\n2,3")).toBe(6)
+        expect(add("1\n2,3")).toBe(6)
     })
     it('should allow newlines',function(){
-        expect(stringCalculator.add("//;\n1;2")).toBe(3)
+        expect(add("//;\n1;2")).toBe(3)
     })
     it('should allow newlines',function(){
-        expect(stringCalculator.add("//4\n142")).toBe(3)
+        expect(add("//4\n142")).toBe(3)
     })
     it('should allow newlines',function(){
-        expect(function(){stringCalculator.add("-1,-2,3,4")}).toThrow(new Error("negatives not allowed -1,-2"))
+        expect(function(){add("-1,-2,3,4")}).toThrow(new Error("negatives not allowed -1,-2"))
     })
     it('should allow newlines',function(){
-        expect(stringCalculator.add("//;\n1000;1;2")).toBe(3)
+        expect(add("//;\n1000;1;2")).toBe(3)
     })
     it('should allow newlines',function(){
-        expect(stringCalculator.add("//***\n1***2***3")).toBe(6)
+        expect(add("//***\n1***2***3")).toBe(6)
     })
-    it('should allow newlines',function(){
-        expect(stringCalculator.add("//[:D][%]\n1:D2%3")).toBe(6)
-    })
-    it('should allow newlines',function(){
-        expect(stringCalculator.add("//[***][%%%]\n1***2%%%3")).toBe(6)
-    })
-    it('should allow newlines',function(){
-        expect(stringCalculator.add("//[(-_-')][%]\n1(-_-')2%3")).toBe(0)
-    })
-    it('should allow newlines',function(){
-        expect(stringCalculator.add("//[abc][777][:(]\n1abc27773:(1")).toBe(7)
-    })
-
-
-
-
-
 
 })
--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,60 +1,33 @@
-const stringCalculator = (function(){
-    function add(str){
-      if (str === ""){
-        return 0
-      }
-      const delimiter = getDelimiter(str)
-      const formattedInput = formatInput(str)
-      return calculateSum(getNumbers(formattedInput, delimiter)) 
+function    add(str){
+    var numbers = [];
+    var expression = "";
+    var delimeter = "";
+    if(str == ""){
+        return(0);
     }
-    function formatInput(str){
-      const delimiterRegex = /^(\/\/.*\n)/
-      const matches = delimiterRegex.exec(str)
-      if(matches && matches.length > 0){
-        return str.replace(delimiterRegex,"")
-      }
-      return str
+    if(str.length == 1){
+        return (parseInt(str));
     }
-    function getDelimiter(str) {
-      const delimiters = []
-      const multipleDelimiterRegexp = /(?:^\/\/)?\[([^\[\]]+)\]\n?/g
-      let matches = multipleDelimiterRegexp.exec(str)
-      for (let i = 0; matches !== null; i++) {
-        delimiters.push(matches[1])
-       matches = multipleDelimiterRegexp.exec(str)  
-      }
-      if(delimiters.length > 0){
-        return new RegExp("["+delimiters.join("")+"]")
-      }
-      matches = (/^\/\/(.*?)\n/g).exec(str)
-      if(matches && matches[1]){
-        return matches[1]
-      }
-      return /[\n,]/
+    if(str.match("//") && !isNaN(parseInt(str.charAt(str.length - 1)))){
+        expression = str.slice(str.search("\n") + 1, str.length);
+        delimeter = str.slice(2, str.search("\n"));
+        numbers = expression.split(delimeter);
     }
-    function getNumbers(string, delimiter){
-      return string.split(delimiter)
-        .filter(n => n !== "")
-        .map(n => parseInt(n))
+    else if(str.match(/[\n]/) && /\d/.test(str[str.length - 1])
+    && /\d/.test(str[0]) && str.match(/[\/]/g) == null ){
+        numbers = str.split("\n").toString();
+        numbers = numbers.split(",");
     }
-    function calculateSum(numbers){
-      const negatives = []
-      const finalSum = numbers.reduce((sum, n) =>{
-        if(n >= 1000){
-          return 0
-        }
-        if(n < 0){
-          negatives.push(n)
-          return 0
-        }
-        return sum + n
-      },0)
-      if(negatives.length > 0){
-        throw Error("negatives not allowed -1,-2")
-      }
-      return finalSum
+    else if(/^[\d,-\d]*$/.test(str)){
+        numbers = str.split(",");
     }
-    return {add}
-}())
-console.log(stringCalculator.add(""))
-module.exports = {stringCalculator}   
+    if(numbers.toString().match(/-\d/)){
+        throw Error("negatives not allowed " + numbers.toString().match(/-\d/g));
+    }
+    return(numbers.filter(function ignore(num) {
+        return num < 1000;
+    }).reduce((sum, current)=> parseInt(sum) + parseInt(current)));
+}
+
+console.log(add("//;\n1000;1;2"));
+module.exports = {add}
