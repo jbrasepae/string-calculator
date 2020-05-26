ec2691833fad0c6eaf9f515040f824f4ddb33af9
Jeanette <jbrasepae@gmail.com>
2020-05-26 13:38:08 +0200

    spec corrected

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -4,31 +4,31 @@ describe('add', function(){
     it('should return 0 for an empty string',function(){
         expect(add("")).toBe(0)
     })
-    it('should input one number as a string',function(){
+    it('should take one number as a string',function(){
         expect(add("1")).toBe(1)
     })
-    it('should input two numbers as a string',function(){
+    it('should take two numbers as a string',function(){
         expect(add("1,1")).toBe(2)
     })
-    it('should input multiple numbers as a string',function(){
+    it('should take multiple numbers as a string',function(){
         expect(add("1,2,3,4")).toBe(10)
     })
-    it('should allow newlines',function(){
+    it('should allow a different delimiter',function(){
         expect(add("1\n2,3")).toBe(6)
     })
-    it('should allow newlines',function(){
+    it('should allow a different delimiter',function(){
         expect(add("//;\n1;2")).toBe(3)
     })
-    it('should allow newlines',function(){
+    it('should allow a different delimiter',function(){
         expect(add("//4\n142")).toBe(3)
     })
-    it('should allow newlines',function(){
+    it('should raise an exception if a negative number passed',function(){
         expect(function(){add("-1,-2,3,4")}).toThrow(new Error("negatives not allowed -1,-2"))
     })
-    it('should allow newlines',function(){
+    it('should allow a different delimiter',function(){
         expect(add("//;\n1000;1;2")).toBe(3)
     })
-    it('should allow newlines',function(){
+    it('should allow a different delimiter',function(){
         expect(add("//***\n1***2***3")).toBe(6)
     })
 
